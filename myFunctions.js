000041c5818cae51dbec6e6800920a0e968064ee
julkuvii <t8juvi00@students.oamk.fi>
2019-01-11 12:16:06 +0200

    Created myFunctions.js

--- myFunctions.js
+++ myFunctions.js
@@ -1,4 +1,4 @@
-function loadDoc(page) {
+function loadXMLDoc(page) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
